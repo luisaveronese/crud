bf7af61ac6fad20daad5f34614f32a27c5083031
luisaveronese <luisamoraes2003@gmail.com>
2023-04-27 15:13:10 -0300

    modified:   cadastro_os.php

--- cadastro_os.php
+++ cadastro_os.php
@@ -399,7 +399,7 @@ if(isset($_POST["btngravar"])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Cadastro</title>
+    <title>Cadastro OS</title>
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="assets/sistema.css">
