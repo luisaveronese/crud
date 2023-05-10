39d9918ea9fa2f7bdd788b7b2b878dd42941abcc
luisaveronese <luisamoraes2003@gmail.com>
2023-05-10 13:37:29 -0300

    new file:   assets/favicon-telecontrol.png 	modified:   include/navbar.php

--- include/navbar.php
+++ include/navbar.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/favicon-telecontrol.png" type="image/x-icon">
     <link rel="stylesheet" href="assets/navbar.css" >
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
